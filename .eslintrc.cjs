3cadec19112021b5ba99411db2985cea0f64a1b8
LuizBarcelar <luizbarcelar2014@gmail.com>
2025-05-21 01:05:25 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'eslint-config-prettier'
+  ],
+  rules: {
+    // Regras personalizadas
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
